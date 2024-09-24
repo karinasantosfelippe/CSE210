66bb57aaa1c604b47f507c2b2fadff4ed3d88e8c
karinasantosfelippe <karinad_santos@hotmail.com>
2024-09-24 15:16:35 -0300

    w2 Learning Activity - done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using Learning02;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _company = "Amazon",
+            _jobTitle = "Software Engineer",
+            _startYear = 2010,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job
+        {
+            _company = "BYU",
+            _jobTitle = "Data Scientist",
+            _startYear = 2015,
+            _endYear = 2024
+        };
+
+        Resume resume1 = new Resume{
+            _name = "Karina Felippe"
+        };
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
