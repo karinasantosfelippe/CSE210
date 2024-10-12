a3720a1fadf9fdd6a5f8b7728a3589785cf1df5a
karinasantosfelippe <fel24001@byui.edu>
2024-10-11 22:38:34 -0300

    breathing activity - done

---
+++ prove/Develop05/BreathingActivity.cs
@@ -0,0 +1,25 @@
+namespace Develop05;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+    {
+        _name = "Breathing";
+        _description = "description here.";
+    }
+
+    public void Run()
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine();
+            Console.Write("\nBreath in... ");
+            ShowCountDown(5);
+            Console.Write("\nNow breath out... ");
+            ShowCountDown(6);
+            Console.WriteLine();
+        }
+    }
+}
